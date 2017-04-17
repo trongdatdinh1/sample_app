255e349371e3426c527e09d206b9d9102dc8e287
trongdatdinh1 <trongdatdinh1@gmail.com>
2017-04-17 10:07:50 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "welcome_to_sample_app"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:success] = t "some_thing_went_wrong"
+      redirect_to root_path
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
   end
 end
