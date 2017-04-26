421e9e31b17539bf18382bc313bf03837278069f
trongdatdinh1 <trongdatdinh1@gmail.com>
2017-04-26 16:22:18 +0700

    Add user microposts part 1

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   end
   resources :account_activations, only: [:edit]
   resources :password_resets, except: [:index, :show, :delete]
+  resources :microposts, only: [:create, :destroy]
 end
